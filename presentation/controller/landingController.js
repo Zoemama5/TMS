07a50cf563fbf9990fda77aef4b11895eaad3869
Zoe <agustinhumpreyzoe@gmail.com>
2025-11-08 15:59:05 +1000

    Final Retouches

--- presentation/controller/landingController.js
+++ presentation/controller/landingController.js
@@ -4,3 +4,11 @@ exports.showLandingPage = (req, res) => {
   const viewsPath = path.join(process.cwd(), 'presentation', 'static', 'views');
   res.sendFile(path.join(viewsPath, 'landingpage.html'));
 };
+exports.showDocumentationPage = (req, res) => {
+  const viewsPath = path.join(process.cwd(), 'presentation', 'static', 'views');
+  res.sendFile(path.join(viewsPath, 'documentation.html'));
+};
+exports.showFeaturePage = (req, res) => {
+  const viewsPath = path.join(process.cwd(), 'presentation', 'static', 'views');
+  res.sendFile(path.join(viewsPath, 'features.html'));
+};
--- presentation/routes/landingRoutes.js
+++ presentation/routes/landingRoutes.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { showLandingPage } = require('../controller/landingController');
+const {
+  showLandingPage,
+  showDocumentationPage,
+  showFeaturePage,
+  bi,
+} = require('../controller/landingController');
 
 // GET /
 router.get('/', showLandingPage);
+router.get('/Documentation', showDocumentationPage);
+router.get('/features', showFeaturePage);
 
 module.exports = router;
